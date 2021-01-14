651b4a3571a5c40ca5a21ccf0ef31c90e1378bb3
Patrick McCann <pgm5@st-andrews.ac.uk>
2021-01-14 14:43:23 +0000

    Adds working map

--- src/app/map-modal/map-modal.page.ts
+++ src/app/map-modal/map-modal.page.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Platform, ModalController } from '@ionic/angular';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {
+	GoogleMaps,
+	GoogleMap,
+	GoogleMapsEvent,
+	GoogleMapOptions,
+  Environment
+} from '@ionic-native/google-maps';
 
 @Component({
   selector: 'app-map-modal',
@@ -9,21 +16,49 @@ import { Geolocation } from '@ionic-native/geolocation/ngx';
 })
 export class MapModalPage implements OnInit{
 
-  public latitude: number;
-  public longitude: number;
+  public latitude: number = 56.81692;
+  public longitude: number = -4.18265;
+
+  map: GoogleMap;
 
   constructor(
     public modalController: ModalController,
-    private geolocation: Geolocation
+    private geolocation: Geolocation,
+    private platform: Platform
   ) { }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.platform.ready();
+    await this.loadMap();
     this.geolocation.getCurrentPosition().then((resp) => {
       this.latitude = resp.coords.latitude;
       this.longitude = resp.coords.longitude;
+      this.map.setCameraTarget({lat: this.latitude, lng: this.longitude});
     }).catch((error) => {
       console.log('Error getting location', error);
     });
+  }
+
+	loadMap() {
+
+    // This code is necessary for browser
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'GOOGLE_MAPS_API_KEY',
+      'API_KEY_FOR_BROWSER_DEBUG': 'GOOGLE_MAPS_API_KEY'
+    });
+
+    const mapOptions: GoogleMapOptions = {
+      camera: {
+         target: {
+           lat: this.latitude,
+           lng: this.longitude
+         },
+         zoom: 12,
+       }
+    };
+
+    this.map = GoogleMaps.create('map_canvas', mapOptions);
+
   }
 
   dismiss() {
