637858612cc100609c22c1c1ba57fade9b483690
Patrick McCann <pgm5@st-andrews.ac.uk>
2021-03-25 09:43:57 +0000

    Removes leafletjs

--- src/app/map-modal/map-modal.page.ts
+++ src/app/map-modal/map-modal.page.ts
@@ -1,23 +1,23 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Platform, ModalController } from '@ionic/angular';
 import { Geolocation } from '@ionic-native/geolocation/ngx';
-import * as Leaflet from 'leaflet';
-import 'leaflet/dist/images/marker-shadow.png';
-import 'leaflet/dist/images/marker-icon.png';
-import 'leaflet/dist/images/marker-icon-2x.png';
+// import * as Leaflet from 'leaflet';
+// import 'leaflet/dist/images/marker-shadow.png';
+// import 'leaflet/dist/images/marker-icon.png';
+// import 'leaflet/dist/images/marker-icon-2x.png';
 
 @Component({
   selector: 'app-map-modal',
   templateUrl: './map-modal.page.html',
   styleUrls: ['./map-modal.page.scss'],
 })
-export class MapModalPage implements OnInit{
+export class MapModalPage{
 
   public latitude = 57.76958852557177;
   public longitude = -7.019251515775875;
-  private marker: Leaflet.Marker;
+  // private marker: Leaflet.Marker;
 
-  map: Leaflet.Map;
+  // map: Leaflet.Map;
 
   constructor(
     public modalController: ModalController,
@@ -25,8 +25,8 @@ export class MapModalPage implements OnInit{
     private platform: Platform
   ) { }
 
-  async ngOnInit() {
-    await this.platform.ready().then( async _ => {
+  ionViewDidEnter() {
+    this.platform.ready().then( async _ => {
       await this.geolocation.getCurrentPosition(
         {timeout: 2000}
       ).then((resp) => {
@@ -39,32 +39,37 @@ export class MapModalPage implements OnInit{
     });
   }
 
+  ionViewDidLeave() {
+    this.cleanUp();
+  }
+
   private doMap() {
-    this.map = Leaflet.map('map_canvas').setView(
-      [this.latitude, this.longitude], 11
-    );
-    this.marker = Leaflet.marker(
-      [this.latitude, this.longitude]
-    ).addTo(this.map);
-    this.map.on('click', e => {
-      this.latitude = e.latlng.lat;
-      this.longitude = e.latlng.lng;
-      this.map.removeLayer(this.marker);
-      this.marker = Leaflet.marker(
-        [e.latlng.lat, e.latlng.lng]
-      ).addTo(this.map);
-    });
-    Leaflet.tileLayer(
-      'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        maxZoom: 19,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-      }
-    ).addTo(this.map);
+    // this.map = Leaflet.map('map_canvas').setView(
+    //   [this.latitude, this.longitude], 11
+    // );
+    // this.marker = Leaflet.marker(
+    //   [this.latitude, this.longitude]
+    // ).addTo(this.map);
+    // this.map.on('click', e => {
+    //   this.latitude = e.latlng.lat;
+    //   this.longitude = e.latlng.lng;
+    //   this.map.removeLayer(this.marker);
+    //   this.marker = Leaflet.marker(
+    //     [e.latlng.lat, e.latlng.lng]
+    //   ).addTo(this.map);
+    // });
+    // Leaflet.tileLayer(
+    //   'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    //     maxZoom: 19,
+    //     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    //   }
+    // ).addTo(this.map);
   }
 
   dismiss() {
     // using the injected ModalController this page
     // can 'dismiss' itself and optionally pass back data
+    this.cleanUp();
     this.modalController.dismiss({
       submitted: false
     });
@@ -73,11 +78,17 @@ export class MapModalPage implements OnInit{
   submitLocation() {
     // using the injected ModalController this page
     // can 'dismiss' itself and optionally pass back data
+    this.cleanUp();
     this.modalController.dismiss({
       submitted: true,
       latitude: this.latitude,
       longitude: this.longitude
     });
+  }
+
+  private cleanUp() {
+    // if (this.map != null) {
+    // }
   }
 
 }
